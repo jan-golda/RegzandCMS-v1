de4639ad4e6502e5f084cda7a813a926543d1228
regzand <regzand@gmail.com>
2015-03-30 20:34:05 +0200

    Created

---
+++ models/Post.js
@@ -0,0 +1,21 @@
+// private modules
+var database	             = requireLocal('database');
+var requiredPlugin          = requireLocal('database/plugins/required');
+var uniquePlugin            = requireLocal('database/plugins/unique');
+var lastModifiedPlugin      = requireLocal('database/plugins/lastModified');
+
+// Post model
+var Post = database.Schema({
+    id:             {type: String, required: true, unique: true},
+    title:          {type: String, required: true},
+    author:         {type: database.Schema.Types.ObjectId, ref: "Users"},
+    created:        {type: Data, default: Date.now}
+});
+
+// attach plugins
+Post.plugin(requiredPlugin);
+Post.plugin(uniquePlugin);
+Post.plugin(lastModifiedPlugin);
+
+// exports
+module.exports = database.model('Post', Post);
